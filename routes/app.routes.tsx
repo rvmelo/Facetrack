8f28154a89a82626eb27a7bdcfc10d522198aa8d
rvmelo <robertomelo7@hotmail.com>
2021-09-09 15:35:50 -0300

    feat: add notifications

--- routes/app.routes.tsx
+++ routes/app.routes.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Alert } from 'react-native';
+
+// import * as Notifications from 'expo-notifications';
 
 import {
   createDrawerNavigator,
@@ -10,9 +13,16 @@ import {
 } from '@react-navigation/drawer';
 
 import { Ionicons } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+
+//  redux
+import { useSelector } from 'react-redux';
+import { IState } from '../store';
+import { IUserState } from '../store/modules/user/types';
 
 //  navigation
-import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 
 //  navigators
 import ProfileRoutes from './profile.routes';
@@ -31,6 +41,9 @@ import RateScreen from '../screens/rateScreen';
 
 // i18n
 import { translate } from '../i18n/src/locales';
+import { registerForPushNotificationsAsync } from '../services/notification';
+import { notificationTokenKey } from '../constants/storage';
+import api from '../services/api';
 
 const AppDrawer = createDrawerNavigator();
 
@@ -63,83 +76,136 @@ const CustomDrawerContent: React.FC<DrawerContentComponentProps> = props => {
   );
 };
 
-const AppDrawerRoutes: React.FC = () => (
-  <AppDrawer.Navigator
-    initialRouteName="Profile"
-    drawerContent={(props: DrawerContentComponentProps) => (
-      <CustomDrawerContent {...props} />
-    )}
-    // drawerContentOptions={{
-    //   activeTintColor: Colors.primary,
-    //   labelStyle: {
-    //     color: Colors.accent,
-    //     fontSize: fonts.sizes.md,
-    //     fontFamily: fonts.family,
-    //   },
-    //   style: {
-    //     backgroundColor: Colors.background,
-    //     paddingTop: 20,
-    //   },
-    // }}
-    screenOptions={() => ({
-      headerShown: true,
-      headerStyle: { backgroundColor: Colors.background },
-      headerTitleStyle: { color: Colors.accent, fontFamily: fonts.family },
-      headerTintColor: Colors.accent,
+const AppDrawerRoutes: React.FC = () => {
+  const { user } = useSelector<IState, IUserState>(state => state.user);
 
-      // activeTintColor: Colors.primary,
-      // labelStyle: {
-      //   color: Colors.accent,
-      //   fontSize: fonts.sizes.md,
-      //   fontFamily: fonts.family,
-      // },
-      // style: {
-      //   backgroundColor: Colors.background,
-      //   paddingTop: 20,
-      // },
-    })}
-  >
-    <AppDrawer.Screen
-      name=" Profile"
-      component={ProfileRoutes}
-      options={({ route }) => ({
-        drawerIcon: () => (
-          <Ionicons name="md-person-outline" size={25} color={Colors.accent} />
-        ),
-        headerShown:
-          getFocusedRouteNameFromRoute(route) !== 'Publication' &&
-          getFocusedRouteNameFromRoute(route) !== 'EditProfile',
-        headerTitle: translate('myProfile'),
-        drawerLabel: translate('myProfile'),
+  // const notificationListener = useRef<any>();
+  // const responseListener = useRef<any>();
+
+  // useEffect(() => {
+  //   notificationListener.current =
+  //     Notifications.addNotificationReceivedListener(notification => {
+  //       console.log('notification received: ', notification);
+  //     });
+
+  //   responseListener.current =
+  //     Notifications.addNotificationResponseReceivedListener(response => {
+  //       console.log(response);
+  //     });
+
+  //   return () => {
+  //     Notifications.removeNotificationSubscription(
+  //       notificationListener.current,
+  //     );
+  //     Notifications.removeNotificationSubscription(responseListener.current);
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const storedNotificationToken = await AsyncStorage.getItem(
+          notificationTokenKey,
+        );
+        if (storedNotificationToken) return;
+        const notificationToken = await registerForPushNotificationsAsync();
+
+        if (notificationToken) {
+          AsyncStorage.setItem(notificationTokenKey, notificationToken);
+        }
+
+        await api.post('/permissions', {
+          notificationToken,
+          userProviderId: user.userProviderId,
+        });
+      } catch (err) {
+        Alert.alert('Error', 'No permission to receive notifications');
+      }
+    })();
+  }, [user.userProviderId]);
+
+  return (
+    <AppDrawer.Navigator
+      initialRouteName="Profile"
+      drawerContent={(props: DrawerContentComponentProps) => (
+        <CustomDrawerContent {...props} />
+      )}
+      // drawerContentOptions={{
+      //   activeTintColor: Colors.primary,
+      //   labelStyle: {
+      //     color: Colors.accent,
+      //     fontSize: fonts.sizes.md,
+      //     fontFamily: fonts.family,
+      //   },
+      //   style: {
+      //     backgroundColor: Colors.background,
+      //     paddingTop: 20,
+      //   },
+      // }}
+      screenOptions={() => ({
+        headerShown: true,
+        headerStyle: { backgroundColor: Colors.background },
+        headerTitleStyle: { color: Colors.accent, fontFamily: fonts.family },
+        headerTintColor: Colors.accent,
+
+        // activeTintColor: Colors.primary,
+        // labelStyle: {
+        //   color: Colors.accent,
+        //   fontSize: fonts.sizes.md,
+        //   fontFamily: fonts.family,
+        // },
+        // style: {
+        //   backgroundColor: Colors.background,
+        //   paddingTop: 20,
+        // },
       })}
-    />
-    <AppDrawer.Screen
-      name="Rate Screen"
-      component={RateScreen}
-      options={() => ({
-        drawerIcon: () => (
-          <Ionicons name="md-globe" size={25} color={Colors.accent} />
-        ),
-        headerTitle: 'Rate Screen',
-        drawerLabel: 'Rate Screen',
-      })}
-    />
-    <AppDrawer.Screen
-      name=" Settings"
-      component={SettingsScreen}
-      options={() => ({
-        drawerIcon: () => (
-          <Ionicons
-            name="md-settings-outline"
-            size={25}
-            color={Colors.accent}
-          />
-        ),
-        headerTitle: translate('settings'),
-        drawerLabel: translate('settings'),
-      })}
-    />
-  </AppDrawer.Navigator>
-);
+    >
+      <AppDrawer.Screen
+        name=" Profile"
+        component={ProfileRoutes}
+        options={({ route }) => ({
+          drawerIcon: () => (
+            <Ionicons
+              name="md-person-outline"
+              size={25}
+              color={Colors.accent}
+            />
+          ),
+          headerShown:
+            getFocusedRouteNameFromRoute(route) !== 'Publication' &&
+            getFocusedRouteNameFromRoute(route) !== 'EditProfile',
+          headerTitle: translate('myProfile'),
+          drawerLabel: translate('myProfile'),
+        })}
+      />
+      <AppDrawer.Screen
+        name="Rate Screen"
+        component={RateScreen}
+        options={() => ({
+          drawerIcon: () => (
+            <Ionicons name="md-globe" size={25} color={Colors.accent} />
+          ),
+          headerTitle: 'Rate Screen',
+          drawerLabel: 'Rate Screen',
+        })}
+      />
+      <AppDrawer.Screen
+        name=" Settings"
+        component={SettingsScreen}
+        options={() => ({
+          drawerIcon: () => (
+            <Ionicons
+              name="md-settings-outline"
+              size={25}
+              color={Colors.accent}
+            />
+          ),
+          headerTitle: translate('settings'),
+          drawerLabel: translate('settings'),
+        })}
+      />
+    </AppDrawer.Navigator>
+  );
+};
 
 export default AppDrawerRoutes;
