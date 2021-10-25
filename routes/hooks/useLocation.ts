1e041fb157b68552452dee966022360504f24d9c
rvmelo <robertomelo7@hotmail.com>
2021-10-25 19:07:50 -0300

    refactor: replace background tracking by foreground tracking

--- routes/hooks/useLocation.ts
+++ routes/hooks/useLocation.ts
@@ -3,7 +3,7 @@ import { Alert } from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import * as TaskManager from 'expo-task-manager';
+// import * as TaskManager from 'expo-task-manager';
 import * as Location from 'expo-location';
 
 //  hooks
@@ -16,80 +16,128 @@ import api from '../../services/api';
 //  i18n
 import { translate } from '../../i18n/src/locales';
 
-interface LocationCoordsArray {
-  locations: Location.LocationObject[];
-}
+// interface LocationCoordsArray {
+//   locations: Location.LocationObject[];
+// }
 
 export function useLocation(): void {
   const { signOut } = useAuth();
 
-  const backgroundTask = 'TRACK-USER';
+  // const backgroundTask = 'TRACK-USER';
 
   const onPermissionsCheck = useCallback(async () => {
     const { status: foregroundStatus } =
       await Location.getForegroundPermissionsAsync();
 
-    const { status: backgroundStatus } =
-      await Location.requestBackgroundPermissionsAsync();
+    // const { status: backgroundStatus } =
+    //   await Location.requestBackgroundPermissionsAsync();
 
-    if (foregroundStatus !== 'granted' || backgroundStatus !== 'granted') {
+    if (foregroundStatus !== 'granted') {
       Alert.alert('Error', translate('locationPermissionError'), [
         { text: 'Ok', onPress: () => signOut() },
       ]);
     }
   }, [signOut]);
 
-  const onBackgroundUpdate = useCallback(async () => {
-    try {
-      TaskManager.defineTask(backgroundTask, async ({ data, error }) => {
-        if (error) {
-          // check `error.message` for more details.
-          return;
-        }
-
-        const { locations } = data as LocationCoordsArray;
-
-        const item = await AsyncStorage.getItem(userLocationKey);
-
-        if (!item) {
-          await AsyncStorage.setItem(
-            userLocationKey,
-            JSON.stringify({ coords: locations[0].coords }),
-          );
-          await api.patch('users/update-location', {
-            coords: locations[0].coords,
-          });
-          return;
-        }
-
-        const storedLocation = JSON.parse(item);
-        const { coords } = storedLocation;
-
-        const { latitude, longitude } = locations[0].coords;
-
-        if (coords.longitude === longitude && coords.latitude === latitude)
-          return;
-
-        await api.patch('users/update-location', {
-          coords: { longitude, latitude },
-        });
-      });
-
-      await Location.startLocationUpdatesAsync(backgroundTask, {
+  const onForegroundUpdate = useCallback(async () => {
+    Location.watchPositionAsync(
+      {
         accuracy: Location.Accuracy.Balanced,
         timeInterval: 60 * 1000,
         distanceInterval: 100,
-      });
-    } catch (err) {
-      Alert.alert('Error', `${translate('backgroundTrackingError')}: ${err}`);
-    }
+      },
+      async data => {
+        try {
+          const { coords } = data as Location.LocationObject;
+
+          const item = await AsyncStorage.getItem(userLocationKey);
+
+          if (!item) {
+            await AsyncStorage.setItem(
+              userLocationKey,
+              JSON.stringify({ coords }),
+            );
+            await api.patch('users/update-location', {
+              coords,
+            });
+            return;
+          }
+
+          const storedLocation = JSON.parse(item);
+          const { coords: storedCoords } = storedLocation;
+
+          const { latitude, longitude } = coords;
+
+          if (
+            storedCoords.longitude === longitude &&
+            storedCoords.latitude === latitude
+          )
+            return;
+
+          await api.patch('users/update-location', {
+            coords: { longitude, latitude },
+          });
+        } catch (err) {
+          Alert.alert(
+            'Error',
+            `${translate('backgroundTrackingError')}: ${err}`,
+          );
+        }
+      },
+    );
   }, []);
+
+  // const onBackgroundUpdate = useCallback(async () => {
+  //   try {
+  //     TaskManager.defineTask(backgroundTask, async ({ data, error }) => {
+  //       if (error) {
+  //         // check `error.message` for more details.
+  //         return;
+  //       }
+
+  //       const { locations } = data as LocationCoordsArray;
+
+  //       const item = await AsyncStorage.getItem(userLocationKey);
+
+  //       if (!item) {
+  //         await AsyncStorage.setItem(
+  //           userLocationKey,
+  //           JSON.stringify({ coords: locations[0].coords }),
+  //         );
+  //         await api.patch('users/update-location', {
+  //           coords: locations[0].coords,
+  //         });
+  //         return;
+  //       }
+
+  //       const storedLocation = JSON.parse(item);
+  //       const { coords } = storedLocation;
+
+  //       const { latitude, longitude } = locations[0].coords;
+
+  //       if (coords.longitude === longitude && coords.latitude === latitude)
+  //         return;
+
+  //       await api.patch('users/update-location', {
+  //         coords: { longitude, latitude },
+  //       });
+  //     });
+
+  //     await Location.startLocationUpdatesAsync(backgroundTask, {
+  //       accuracy: Location.Accuracy.Balanced,
+  //       timeInterval: 60 * 1000,
+  //       distanceInterval: 100,
+  //     });
+  //   } catch (err) {
+  //     Alert.alert('Error', `${translate('backgroundTrackingError')}: ${err}`);
+  //   }
+  // }, []);
 
   useEffect(() => {
     onPermissionsCheck();
   }, [onPermissionsCheck]);
 
   useEffect(() => {
-    onBackgroundUpdate();
-  }, [onBackgroundUpdate]);
+    onForegroundUpdate();
+  }, [onForegroundUpdate]);
 }
